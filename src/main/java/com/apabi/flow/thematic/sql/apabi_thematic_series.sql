7282de62df375ec8e3f7d10d618ecf0dcf5f9ba9
pirui <pirui@HOLD.FOUNDER.COM>
2018-10-18 15:16:51 +0800

    删除原有项目

--- src/main/java/com/apabi/flow/thematic/sql/apabi_thematic_series.sql
+++
@@ -1,9 +0,0 @@
-create table book.apabi_thematic_series(
-    ID varchar2(64),
-    TITLE varchar2(500),
-    DATASOURCE varchar2(500),
-    COLLATOR varchar2(100),
-    OPERATOR varchar2(100),
-    CREATETIME date,
-    UPDATETIME date
-);
