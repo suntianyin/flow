11fb025ac8938c1e9a6582e7959203e4f1caa29a
suntianyin <s971435397@163.com>
2019-01-24 18:04:40 +0800

    书目解析优化识别，出版社搜索解决

--- src/main/java/com/apabi/flow/processing/service/impl/BibliothecaServiceImpl.java
+++ src/main/java/com/apabi/flow/processing/service/impl/BibliothecaServiceImpl.java
@@ -1141,7 +1141,8 @@ public class BibliothecaServiceImpl implements BibliothecaService {
         try {
             ArrayList<File> fileList = new ArrayList<>();
             ArrayList<File> files = getFiles(fileList, path);
-            ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 200, TimeUnit.MILLISECONDS,
+            int cpuSum = Runtime.getRuntime().availableProcessors();
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(cpuSum*2, cpuSum*2, 200, TimeUnit.MILLISECONDS,
                     new ArrayBlockingQueue<Runnable>(files.size()));
 
             for (int i = 1; i <= files.size(); i++) {
