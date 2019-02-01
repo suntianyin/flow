9085f59d40f1b778d02be596c1b969d3ddfedcd4
suntianyin <s971435397@163.com>
2019-02-01 11:35:45 +0800

    书目解析还原

--- src/main/java/com/apabi/flow/processing/service/impl/MyTask.java
+++ src/main/java/com/apabi/flow/processing/service/impl/MyTask.java
@@ -35,17 +35,17 @@ import java.util.regex.Pattern;
 public class MyTask implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(MyTask.class);
 
-//    private int i;
-    private ArrayBlockingQueue<File> idQueue;
+    private int i;
+    private File f;
     private String username;
     private String batchId;
     private PublisherDao publisherDao;
     private BibliothecaMapper bibliothecaMapper;
     private ApplicationConfig config;
 
-    public MyTask( ArrayBlockingQueue idQueue, String username, String batchId, PublisherDao publisherDao, BibliothecaMapper bibliothecaMapper, ApplicationConfig config) {
-//        this.i = i;
-        this.idQueue = idQueue;
+    public MyTask( int i,File f, String username, String batchId, PublisherDao publisherDao, BibliothecaMapper bibliothecaMapper, ApplicationConfig config) {
+        this.i = i;
+        this.f = f;
         this.username = username;
         this.batchId = batchId;
         this.publisherDao = publisherDao;
@@ -55,7 +55,7 @@ public class MyTask implements Runnable {
 
     @Override
     public void run() {
-//        logger.info("正在执行task " + i);
+        logger.info("正在执行task " + i);
         Bibliotheca bibliotheca = new Bibliotheca();
         String isbn = "";
         String edition = "";
@@ -66,9 +66,7 @@ public class MyTask implements Runnable {
         String publishTime = "";
         String paperPrice = "";
         boolean a = true;
-        File f= null;
         try {
-            f = idQueue.take();
             File file = new File(config.getTargetCopyRightDir() + File.separator + batchId);
             if (!file.exists()) {
                 file.mkdirs();
@@ -257,7 +255,7 @@ public class MyTask implements Runnable {
             Date date = new Date();
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
             String d = simpleDateFormat.format(date);
-            bibliotheca.setIdentifier(d + batchId + f.getName());//putong(i++)
+            bibliotheca.setIdentifier(d + batchId + putong(i++));//
             bibliotheca.setDocumentFormat("pdf");
             bibliotheca.setBatchId(batchId);
             bibliotheca.setEdition(edition);
@@ -270,7 +268,7 @@ public class MyTask implements Runnable {
             }
             bibliothecaMapper.insertSelective(bibliotheca);
         }
-//        logger.info("task " + i + "执行完毕");
+        logger.info("task " + i + "执行完毕");
     }
 
     //编号
